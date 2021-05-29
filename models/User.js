4bf4b480b619c096305046f7b5a182c481a60cbe
Megs <Megs.close@gmail.com>
2021-05-29 15:42:07 -0700

    user schema finished

--- models/User.js
+++ models/User.js
@@ -2,11 +2,44 @@ const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
     username: {
-        type: String
+        type: String,
+        required: "You must provide a username",
+        trim: true
     },
     email: {
-        type: String
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        },
+        required: "You must provide a valid email",
+        unique: true
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
     }
+);
+
+UserSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
 });
 
 // create the User model using the UserSchema
