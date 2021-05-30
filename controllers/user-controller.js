9b6844e2508b22d1ff2db271447212b49b9db346
Megs <Megs.close@gmail.com>
2021-05-30 15:29:40 -0700

    not sure-must test this route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -73,13 +73,44 @@ const userController = {
             .then(() => {
                 // remove user comments
                 Thought.deleteMany({ username: dbUserData.username })
-                .then(() => {
-                    res.json({ message: "Deleted user!"});
-                })
-                .catch(err => res.status(400).json(err));
+                    .then(() => {
+                        res.json({ message: "Deleted user!" });
+                    })
+                    .catch(err => res.status(400).json(err));
             })
             .catch(err => res.status(400).json(err));
-    }
+    },
+
+    // POST add a friend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: params.friendId } },
+            { new: true, runValidators: true }
+        )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "No User found with this id" });
+                    return;
+                }
+                // need to add this user to friend's list as well
+                User.findOneAndUpdate(
+                    { _id: params.friendId },
+                    { $addToSet: { friends: params.userId } },
+                    { new: true, runValidators: true }
+                )
+                    .then(dbFriendData => {
+                        if (!dbFriendData) {
+                            res.status(404).json({ message: "No User found with this id" });
+                            return;
+                        }
+                        res.json(dbUserData);
+                    })
+                    .catch(err => res.status(400).json(err));
+            })
+            .catch(err => res.status(400).json(err));
+    },
 };
+
 
 module.exports = userController;
