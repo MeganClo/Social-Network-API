f2225abc6c397ca26467f05a2fc49c8904cf4651
Megs <Megs.close@gmail.com>
2021-06-01 20:15:44 -0700

    trying thought-routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -10,3 +10,20 @@ const {
     removeThought,
 } = require("../../controllers/thought-controller");
 
+router
+    .route("/")
+    .get(getAllThoughts)
+    .post(addThought);
+
+router
+    .route("/:id")
+    .get(getThoughtById)
+    .delete(removeThought)
+    .put(updateThought);
+
+router
+    .route("/thoughtId/reactions")
+    .post(addReaction)
+    .delete(removeReaction);
+
+module.exports = router;
